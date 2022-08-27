bfd6e96112f4e24538c37794704af27fff0dd203
josuenm <direct.josue@gmail.com>
2022-08-27 17:59:57 -0300

    chore: component removed

--- src/components/Text/index.tsx
+++
@@ -1,6 +0,0 @@
-import { Container } from "./styles";
-import { TextProps } from "./types";
-
-export default ({ children, ...rest }: TextProps) => (
-  <Container {...rest}>{children}</Container>
-);
