50bca4a77cd055801d771848f27655cd3473a354
josuenm <direct.josue@gmail.com>
2022-08-27 02:30:17 -0300

    feat: font-family property

--- src/components/Text/styles.ts
+++ src/components/Text/styles.ts
@@ -2,6 +2,8 @@ import styled from "styled-components/native";
 import { TextStyles } from "./types";
 
 export const Container = styled.Text<TextStyles>`
+  font-family: ${({ fontWeight, theme }) =>
+    theme.fontWeight[fontWeight] || theme.fontWeight["400"]};
   font-size: ${({ fontSize }) => fontSize || "14px"};
   color: ${({ color }) => color || "#fff"};
 `;
