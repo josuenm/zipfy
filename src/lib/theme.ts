9f768f5a1e660b6f3e4cb6abfc0fe9f4f61cf435
josuenm <direct.josue@gmail.com>
2022-08-27 02:26:03 -0300

    feat: theme configuration and type of theme added
    Theme has been configurated with fontWeight property, type of theme added

--- src/lib/theme.ts
+++ src/lib/theme.ts
@@ -1,4 +1,9 @@
+export const theme = {
+  fontWeight: {
+    400: "Inter_400Regular",
+    500: "Inter_500Medium",
+    700: "Inter_700Bold",
+  },
+};
 
-
-
-
+export type ThemeProps = typeof theme;
