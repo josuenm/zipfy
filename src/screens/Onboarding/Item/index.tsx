6fef64ef073c0577bd5e18fbc3f6e9f7d7648082
josuenm <direct.josue@gmail.com>
2022-08-27 20:23:01 -0300

    feat: gif component and info component

--- src/screens/Onboarding/Item/index.tsx
+++ src/screens/Onboarding/Item/index.tsx
@@ -1,10 +1,11 @@
 import { useWindowDimensions } from "react-native";
-import { Container, Description, Title } from "./styles";
+import { Container, Description, Gif, Info, Title } from "./styles";
 
 interface ItemProps {
   id: string;
   title: string;
   description: string;
+  gif: string;
 }
 
 const OnboardingItem = ({ data }: { data: ItemProps }) => {
@@ -12,8 +13,11 @@ const OnboardingItem = ({ data }: { data: ItemProps }) => {
 
   return (
     <Container width={width}>
-      <Title> {data.title} </Title>
-      <Description> {data.description} </Description>
+      <Gif source={data.gif} />
+      <Info>
+        <Title> {data.title} </Title>
+        <Description> {data.description} </Description>
+      </Info>
     </Container>
   );
 };
