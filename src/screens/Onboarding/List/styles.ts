7f6d26fa30117302023a974fd64b7147551a2b1f
josuenm <direct.josue@gmail.com>
2022-08-27 23:29:30 -0300

    feat: button styles
    NextButtonContainer and ButtonWithIcon

--- src/screens/Onboarding/List/styles.ts
+++ src/screens/Onboarding/List/styles.ts
@@ -6,6 +6,23 @@ export const Container = styled.View`
 
 export const List = styled.FlatList.attrs({
   horizontal: true,
-  showsHorizontalScrollIndicator: true,
+  showsHorizontalScrollIndicator: false,
   pagingEnabled: true,
 })``;
+
+export const NextButtonContainer = styled.View`
+  margin-bottom: 100px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonWithIcon = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.6,
+})`
+  position: absolute;
+  background-color: ${({ theme }) => theme.colors.primary};
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  padding: 20px;
+`;
