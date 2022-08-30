7087db2b69381ee36acc062a33bfcd7f27f566ba
josuenm <direct.josue@gmail.com>
2022-08-29 23:54:02 -0300

    feat: address type created

---
+++ src/types/AddressProps.ts
@@ -0,0 +1,18 @@
+export interface AddressProps {
+  id: string;
+  title: string;
+  description: string;
+}
+
+export interface AddressResponseProps {
+  bairro: string;
+  cep: string;
+  complemento: string;
+  ddd: string;
+  gia: string;
+  ibge: string;
+  localidade: string;
+  logradouro: string;
+  siafi: string;
+  uf: string;
+}
