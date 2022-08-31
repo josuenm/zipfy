4a40323c5b0d788682470087f26cd150e62d3c16
josuenm <direct.josue@gmail.com>
2022-08-31 13:11:20 -0300

    feat: bottom Navigation working

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,5 +1,23 @@
+import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Dashboard from "@screens/Dashboard";
+import React from "react";
+import { Item, TabBarStyle, Title } from "./styles";
+
+interface ItemNavigationProps {
+  title: string;
+  icon: "home-outline";
+  active: boolean;
+}
+
+const ItemNavigation = ({ title, icon, active }: ItemNavigationProps) => {
+  return (
+    <Item active={active}>
+      <Ionicons name={icon} size={20} color={active ? "white" : "black"} />
+      {!active && <Title>{title}</Title>}
+    </Item>
+  );
+};
 
 const Tab = createBottomTabNavigator();
 
@@ -8,9 +26,19 @@ export const BottomNavigation = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: TabBarStyle.container,
+        tabBarIconStyle: TabBarStyle.icon,
       }}
     >
-      <Tab.Screen name="Home" component={Dashboard} />
+      <Tab.Screen
+        name="Home"
+        component={Dashboard}
+        options={{
+          tabBarLabel: ({ focused }) => (
+            <ItemNavigation title="Home" icon="home-outline" active={focused} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
