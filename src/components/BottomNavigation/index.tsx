6c36cfc9cbc2c8ef830953dc0cf037f4d0a1322c
josuenm <direct.josue@gmail.com>
2022-08-31 14:03:39 -0300

    style: navigator configuration, new screen added

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,19 +1,20 @@
 import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Dashboard from "@screens/Dashboard";
+import MyAddresses from "@screens/MyAddresses";
 import React from "react";
 import { Item, TabBarStyle, Title } from "./styles";
 
 interface ItemNavigationProps {
   title: string;
-  icon: "home-outline";
+  icon: "home-outline" | "map-outline";
   active: boolean;
 }
 
 const ItemNavigation = ({ title, icon, active }: ItemNavigationProps) => {
   return (
     <Item active={active}>
-      <Ionicons name={icon} size={20} color={active ? "white" : "black"} />
+      <Ionicons name={icon} size={20} color="white" />
       {!active && <Title>{title}</Title>}
     </Item>
   );
@@ -26,16 +27,29 @@ export const BottomNavigation = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
         tabBarStyle: TabBarStyle.container,
-        tabBarIconStyle: TabBarStyle.icon,
       }}
     >
       <Tab.Screen
         name="Home"
         component={Dashboard}
         options={{
-          tabBarLabel: ({ focused }) => (
+          tabBarIcon: ({ focused }) => (
             <ItemNavigation title="Home" icon="home-outline" active={focused} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="MyAddresses"
+        component={MyAddresses}
+        options={{
+          tabBarIcon: ({ focused }) => (
+            <ItemNavigation
+              title="Adresses"
+              icon="map-outline"
+              active={focused}
+            />
           ),
         }}
       />
