a98d07270104ef3330e77cf7378c6b707a323307
josuenm <direct.josue@gmail.com>
2022-08-31 16:29:06 -0300

    feat: new components, components renamed
    AddressContainer, AddressZipCode, AddressTitle, AddressDescription has been created

--- src/components/AddressCard/styles.ts
+++ src/components/AddressCard/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View`
+export const AddressFoundContainer = styled.View`
   border: 1px solid #303030;
   padding: 10px;
   border-radius: 10px;
@@ -11,15 +11,44 @@ export const Item = styled.View`
   align-items: center;
 `;
 
-export const Title = styled.Text`
+export const FoundTitle = styled.Text`
   font-family: ${({ theme }) => theme.fontWeight.bold};
   font-size: 16px;
   color: #fff;
   margin-right: 5px;
 `;
 
-export const Description = styled.Text`
+export const FoundDescription = styled.Text`
   font-family: ${({ theme }) => theme.fontWeight.regular};
   font-size: 14px;
   color: #fff;
+`;
+
+export const AddressContainer = styled.View`
+  border: 1px solid #aaa;
+  padding: 10px;
+  border-radius: 10px;
+`;
+
+export const AddressZipCode = styled.Text`
+  text-align: center;
+  font-family: ${({ theme }) => theme.fontWeight.bold};
+  font-size: 18px;
+  color: #fff;
+  margin-right: 5px;
+  margin-bottom: 10px;
+`;
+
+export const AddressTitle = styled.Text`
+  font-family: ${({ theme }) => theme.fontWeight.bold};
+  font-size: 16px;
+  color: #aaa;
+  margin-right: 5px;
+`;
+
+export const AddressDescription = styled.Text`
+  font-family: ${({ theme }) => theme.fontWeight.medium};
+  font-size: 14px;
+  color: #aaa;
+  margin-right: 5px;
 `;
