8d593104532788db65469ba7ce6b83e531ac6914
josuenm <direct.josue@gmail.com>
2022-08-31 16:23:27 -0300

    feat: data for test, List component added

--- src/screens/MyAddresses/index.tsx
+++ src/screens/MyAddresses/index.tsx
@@ -1,9 +1,54 @@
-import { Container, Title } from "./styles";
+import { Address } from "@components/AddressCard";
+import { AddressList, Container, ItemSeparator, Title } from "./styles";
 
 const MyAddresses = () => {
+  const myAddresses = [
+    {
+      id: "1",
+      cep: "12308530",
+      localidade: "Jacareí",
+      bairro: "Centro",
+      logradouro: "Rua Cônego José Bento",
+    },
+    {
+      id: "2",
+      cep: "12308530",
+      localidade: "Jacareí",
+      bairro: "Centro",
+      logradouro: "Rua Cônego José Bento",
+    },
+    {
+      id: "3",
+      cep: "12308530",
+      localidade: "Jacareí",
+      bairro: "Centro",
+      logradouro: "Rua Cônego José Bento",
+    },
+    {
+      id: "4",
+      cep: "12308530",
+      localidade: "Jacareí",
+      bairro: "Centro",
+      logradouro: "Rua Cônego José Bento",
+    },
+    {
+      id: "5",
+      cep: "12308530",
+      localidade: "Jacareí",
+      bairro: "Centro",
+      logradouro: "Rua Cônego José Bento",
+    },
+  ];
+
   return (
     <Container>
-      <Title>My addresses</Title>
+      <Title>Todos os meus endereços</Title>
+      <AddressList
+        data={myAddresses}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+        renderItem={({ item }) => <Address data={item} />}
+        keyExtractor={(item: { id: string }) => item.id}
+      />
     </Container>
   );
 };
