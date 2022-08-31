1f877c8da0f3245998e4e349efdf9c299d53c2ff
josuenm <direct.josue@gmail.com>
2022-08-31 16:24:27 -0300

    feat: components created, container styled

--- src/screens/MyAddresses/styles.ts
+++ src/screens/MyAddresses/styles.ts
@@ -2,11 +2,25 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
 `;
 
 export const Title = styled.Text`
+  text-align: center;
   color: #fff;
+  font-size: 20px;
+  margin: 35px 0;
   font-family: ${({ theme }) => theme.fontWeight.bold};
+`;
+
+export const ItemSeparator = styled.View`
+  margin: 15px 0;
+`;
+
+export const AddressList = styled.FlatList.attrs({
+  contentContainerStyle: {
+    paddingBottom: 90,
+  },
+})`
+  flex: 1;
+  padding: 0 25px;
 `;
