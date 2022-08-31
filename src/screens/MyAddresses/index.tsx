92e971f9dfe61c0e57eed7896a71ba004a7fe1f4
josuenm <direct.josue@gmail.com>
2022-08-31 14:05:00 -0300

    feat: new screen

---
+++ src/screens/MyAddresses/index.tsx
@@ -0,0 +1,11 @@
+import { Container, Title } from "./styles";
+
+const MyAddresses = () => {
+  return (
+    <Container>
+      <Title>My addresses</Title>
+    </Container>
+  );
+};
+
+export default MyAddresses;
---
+++ src/screens/MyAddresses/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-family: ${({ theme }) => theme.fontWeight.bold};
+`;
