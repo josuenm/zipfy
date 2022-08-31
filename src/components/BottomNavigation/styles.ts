eb1f333153edea3fed943c92a1b55b00749667cf
josuenm <direct.josue@gmail.com>
2022-08-31 13:12:58 -0300

    feat: container style, item style

--- src/components/BottomNavigation/styles.ts
+++ src/components/BottomNavigation/styles.ts
@@ -1,12 +1,39 @@
+import { StyleSheet } from "react-native";
 import styled from "styled-components/native";
 
-export const Item = styled.View`
-  width: 150px;
+export const TabBarStyle = StyleSheet.create({
+  container: {
+    width: "90%",
+    height: 55,
+    position: "absolute",
+    bottom: 10,
+    right: 0,
+    left: "5%",
+    backgroundColor: "rgba(255,255,255,0.3)",
+    borderRadius: 100,
+  },
+  icon: {
+    display: "none",
+  },
+});
+
+export const Item = styled.TouchableOpacity.attrs({
+  activeOpacity: 0.8,
+})<{ active: boolean }>`
+  width: ${({ active }) => "40px"};
+  height: ${({ active }) => "40px"};
+  margin: auto 0;
   align-items: center;
+  justify-content: center;
+  justify-self: center;
+  align-self: center;
+  border-radius: 300px;
+
+  background-color: ${({ active, theme }) => active && theme.colors.primary};
 `;
 
-export const Title = styled.Text`
-  color: #fff;
+export const Title = styled.Text<{ active: boolean }>`
+  color: ${({ active }) => (active ? "#fff" : "#000")};
   font-family: ${({ theme }) => theme.fontWeight.medium};
   font-size: 14px;
 `;
