4f3b47ee6a6ccde2369c338663333fc204a8c95b
josuenm <direct.josue@gmail.com>
2022-08-31 12:02:55 -0300

    style: simple structure

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -1,34 +1,16 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { Dashboard } from "@screens/Dashboard";
-import { Item, Navigator, Title } from "./styles";
-
-import { Ionicons } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { TouchableOpacity } from "react-native";
+import Dashboard from "@screens/Dashboard";
+
 const Tab = createBottomTabNavigator();
 
 export const BottomNavigation = () => {
   return (
-    <NavigationContainer>
-      <Navigator
-        initialRouteName="Dashboard"
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            return (
-              <TouchableOpacity activeOpacity={0.6}>
-                <Item>
-                  <Ionicons name="home-outline" size={20} color="white" />
-                  <Title>Home</Title>
-                </Item>
-              </TouchableOpacity>
-            );
-          },
-          // tabBarActiveTintColor: "tomato",
-          // tabBarInactiveTintColor: "gray",
-        })}
-      >
-        <Tab.Screen name="Dashboard" component={Dashboard} />
-      </Navigator>
-    </NavigationContainer>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tab.Screen name="Home" component={Dashboard} />
+    </Tab.Navigator>
   );
 };
